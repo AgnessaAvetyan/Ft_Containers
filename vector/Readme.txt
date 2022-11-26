cb7a7df86969d33cb270fcec0a1ee4508475270e
AgnessaAvetyan <agnessa.avetyan.01@mail.ru>
2022-11-26 17:49:14 +0400

    Capacity +  Modifiers

--- vector/Readme.txt
+++ vector/Readme.txt
@@ -1,16 +1,60 @@
 //  std::vector
     https://cplusplus.com/reference/vector/vector/?kw=vector
 
-//  std::allocator::allocate
-    https://cplusplus.com/reference/memory/allocator/allocate/
-
 //  std::random_access_iterator_tag
     https://cplusplus.com/reference/iterator/RandomAccessIterator/
 
 //  std::vector::vector (ctor)
     https://cplusplus.com/reference/vector/vector/vector/
 
+    //  std::allocator::allocate
+        https://cplusplus.com/reference/memory/allocator/allocate/
+
+    //  std::allocator::construct
+        https://cplusplus.com/reference/memory/allocator/construct/
+
 //  std::vector::~vector
     https://cplusplus.com/reference/vector/vector/~vector/
-    
 
+    //  std::allocator::destroy
+        https://cplusplus.com/reference/memory/allocator/destroy/
+
+    //  std::allocator::deallocate
+        https://cplusplus.com/reference/memory/allocator/deallocate/
+
+//**//  Capacity:
+
+//  std::vector::size
+    https://cplusplus.com/reference/vector/vector/size/
+
+//  std::vector::max_size
+    https://cplusplus.com/reference/vector/vector/max_size/
+
+//  std::vector::capacity
+    https://cplusplus.com/reference/vector/vector/capacity/
+
+//  std::vector::empty
+    https://cplusplus.com/reference/vector/vector/empty/
+
+//  std::vector::resize
+    https://cplusplus.com/reference/vector/vector/resize/
+
+//  std::vector::reserve
+    https://cplusplus.com/reference/vector/vector/reserve/
+
+//**//  Modifiers:
+
+//  std::vector::assign
+    https://cplusplus.com/reference/vector/vector/assign/
+
+//  std::vector::push_back
+    https://cplusplus.com/reference/vector/vector/push_back/
+
+//  std::vector::pop_back
+    https://cplusplus.com/reference/vector/vector/pop_back/
+
+//  std::vector::pop_back
+    https://cplusplus.com/reference/vector/vector/pop_back/
+
+//  std::vector::insert
+    https://cplusplus.com/reference/vector/vector/insert/
--- vector/vector.hpp
+++ vector/vector.hpp
@@ -7,7 +7,8 @@ namespace ft
     template < class T, class Alloc = std::allocator<T> >
     class vector
     {
-    template <typename It> RandomAccessIterator
+    template <typename It>
+    class RandomAccessIterator
     {
 
     };
@@ -52,16 +53,236 @@ namespace ft
             _c = _s;
             _p = _a.allocate(_c);
             for(difference_type i = 0; i < static_cast<difference_type>(_s); ++i)
-                _a.construct(_p + 1, *(_p + 1));
+                _a.construct(_p + i, *(_p + i));
         }
         vector (const vector& x) 
-        { *this = x; }     
+        { *this = x; }
         ~vector()
         {
-            for(int i = 0; i < _s; ++i)
-                de
-        }      
+            for(size_type i = 0; i < _s; ++i)
+                _a.destroy(_p + i);
+            if (_c)
+                _a.deallocate(_p, _c);
+        }
+
+        size_type size() const { return _s; }
+        size_type max_size() const { return _a.max_size(); }
+        size_type capacity() const { return _c; }
+        bool empty() const { return _s == 0; }
+
+        void resize (size_type n, value_type val = value_type())
+        {
+            if (n < this->_s)
+            {
+                for (size_type i = 0; i < _s; i++)
+                    _a.destroy(_p + i);
+                _s = n;
+            }
+            else if (n > this->_s)
+            {
+                if (n > this->_c)
+                    this->reverse(_c * 2 > n ? _c * 2 : n);
+                for (size_type i = _s; i < n; i++)
+                    _a.construct(_p + i, val);
+            }
+        }
+        void reserve (size_type n)
+        {
+            if (n > max_size())
+                throw std::length_error("The size requested is greater than the maximum size");
+            else if (n < this->capacity())
+                return ;
+            else
+            {
+                pointer vec = _a.allocate(n);
+                for (size_type i = 0; i < _s; i++)
+                    _a.construct(vec + i, *(_p + i));
+                for (size_type i = 0; i < _s; i++)
+                    _a.destroy(_p + i);
+                if (_c)
+                    _a.deallocate(_p, _c);
+                _c = n;
+                _p = vec;
+                // try
+                // {
+                //     for (size_t i = 0; i < _s; i++)
+                //         _a.construct(vec + i, *(_p + i));
+                // }
+                // catch(const std::exception& e)
+                // {
+                //     for (size_t i = 0; vec + i != NULL && i < _s; i++)
+                //         _a.destroy(vec + i);
+                //     _a.deallocate(vec, n);
+                // }
+            }
+        }
+
+        template <class InputIterator>
+        void assign (InputIterator first, InputIterator last)
+        {
+            if (first > last)
+                throw std::logic_error("vector: undefined behavior");
+            difference_type len = last - first;
+            clear();
+            if (len > static_cast<difference_type>(capacity()))
+            {
+                _a.deallocate(_p, _c);
+                _a.allocate(len);
+                _c = len;
+            }
+            iterator it = begin();
+            while (first < last)
+            {
+                _a.construct(&(*it), *first);
+                it++;
+                first++;
+            }
+            _s = len;
+        }
+	
+        void assign (size_type n, const value_type& val)
+        {
+            clear();
+            if (n > _c)
+            {
+                _a.deallocate(_p, _c);
+                _a.allocate(n);
+                _c = n;
+            }
+            for (size_type i = 0; i < n; i++)
+                _a.construct(_p + i, val);
+            _s = n;
+        }
+    
+        void push_back (const value_type& val)
+        {
+            if (_s == _c)
+                reserve(_c == 0 ? 1 : _c * 2);
+            _a.construct(_p + _s, val);
+            ++_s;
+        }
+
+        void pop_back()
+        {
+            if (_s == 0)
+                return ;
+            _a.destroy(_p + _s - 1);
+            _s--;
+        }
+        
+        iterator insert (iterator position, const value_type& val)
+        {
+            if (position < this->begin() || position > this->end()) 
+                throw std::logic_error("vector: undefined behavior");
+            if (_s == _c)
+            {
+                _c = _c * 2 + (_c == 0);
+                pointer vec = _a.allocate(_c);
+                std::uninitialized_copy(begin(), position, iterator(vec));
+                _a.construct(vec + position - begin(), val);
+                std::uninitialized_copy(position, end(), iterator(vec + position - begin() + 1));
+                for(size_type i = 0; i < _s; ++i)
+                    _a.destroy(_p + i);
+                if (_c)
+                    _a.deallocate(_p, _c);
+                _s++;
+                _p = vec;
+            }
+            else
+            {
+                for (size_type i = _s; i < static_cast<size_type>(position - begin()); i++)
+                {
+                    _a.destroy(_p + i);
+                    _a.construct(_p + i, *(_p + i - 1));
+                }
+                _a.destroy(&(*position));
+                _a.construct(&(*position), val);
+                ++s;             
+            }
+            return position;
+        }
+
+        void insert (iterator position, size_type n, const value_type& val)
+        {
+            if (position < this->begin() || position > this->end()) 
+                throw std::logic_error("vector: undefined behavior");
+            if (n == 0)
+                return ;
+            else if (max_size() - _s < n)
+                throw std::length_error("vector: undefined behavior");
+            iterator beg = position - begin();
+            if (_s + n > _c)
+            {
+                _c = (_c * 2 > _s + n ? _c * 2 : _s + n);
+                pointer vec = _a.allocate(_c);
+                std::uninitialized_copy(begin(), position, iterator(vec));
+                for (size_type i = 0; i < n; i++)
+                    _a.construct(vec + beg + i, val);
+                std::uninitialized_copy(position, end(), iterator(vec + beg + n));
+                for(size_type i = 0; i < _s; ++i)
+                    _a.destroy(_p + i);
+                if (_c)
+                    _a.deallocate(_p, _c);
+                _p = vec;
+            }
+            else
+            {
+                for (size_type i = _s; i < static_cast<size_type>(beg); i++)
+                {
+                    _a.destroy(_p + n + i - 1);
+                    _a.construct(_p + n + i - 1, *(_p + i - 1));
+                }
+                for (size_type i = 0; i < n; i++)
+                {
+                    _a.destroy(_p + beg + i);
+                    _a.construct(_p + beg + i, val);
+                }
+            }
+            _s += n;
+        }
+
+        template <class InputIterator>
+        void insert (iterator position, InputIterator first, InputIterator last)
+        {
+            if (position < begin() || position > end() || first > last)
+                throw std::logic_error("vector: undefined behavior");
+            size_type len = static_cast<size_type>(last - first);
+            iterator beg = position - begin();
+            if (_s + len > _c)
+            {
+                _c = (_c * 2 > _s + len ? _c * 2 : _s + len);
+                pointer vec = _a.allocate(_c);
+                std::uninitialized_copy(begin(), position, iterator(vec));
+                for (size_type i = 0; i < len; i++)
+                {
+                    _a.construct(vec + beg + i, *first);
+                    first++;
+                }
+                std::uninitialized_copy(position, end(), iterator(vec + beg + len));
+                for(size_type i = 0; i < _s; ++i)
+                    _a.destroy(_p + i);
+                if (_c)
+                    _a.deallocate(_p, _c);
+                _p = vec;
+            }
+            else
+            {
+                for (size_type i = _s; i < static_cast<size_type>(beg); i++)
+                {
+                    _a.destroy(_p + len + i - 1);
+                    _a.construct(_p + len + i - 1, *(_p + i - 1));
+                }
+                for (size_type i = 0; i < n; i++)
+                {
+                    _a.destroy(_p + beg + i);
+                    _a.construct(_p + beg + i, *first);
+                    first++;
+                }
+            }
+            _s += len;
+        }
     };
-}
+
+} // namespace ft
 
 #endif // VECTOR_HPP
